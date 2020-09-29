d4b823330d1cc88788a071e33193e63975e63b9b
Hesperis86 <monikaw1986@go2.pl>
2020-09-29 14:05:56 +0200

    Add recipe activity
    Data input types needing correction.

---
+++ app/src/main/java/com/example/zeszytmojejmamy/AddRecipeActivity.java
@@ -0,0 +1,77 @@
+package com.example.zeszytmojejmamy;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AddRecipeActivity extends AppCompatActivity {
+    public static final String EXTRA_TITLE = "com.example.zeszytmojejmamy.EXTRA_TITLE";
+    public static final String EXTRA_PORTIONS_NUM = "com.example.zeszytmojejmamy.EXTRA_PORTIONS_NUM";
+    public static final String EXTRA_INGREDIENTS = "com.example.zeszytmojejmamy.EXTRA_INGREDIENTS";
+    public static final String EXTRA_PREP_INSTRUCTIONS = "com.example.zeszytmojejmamy.EXTRA_PREP_INSTRUCTIONS";
+
+    private EditText editTextTitle;
+    private EditText editTextPortions;
+    private EditText editTextIngredients;
+    private EditText editTextPreparationInstructions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_recipe);
+
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextPortions = findViewById(R.id.edit_text_portions);
+        editTextIngredients = findViewById(R.id.edit_text_ingredients);
+        editTextPreparationInstructions = findViewById(R.id.edit_text_preparationInstructions);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle(getString(R.string.addRecipe));
+    }
+
+    private void saveRecipe() {
+        String title = editTextTitle.getText().toString();
+        String portionNumber = editTextPortions.getText().toString();
+        //int portionNumber = editTextPortions.toString().getValue();
+        String ingredients = editTextIngredients.getText().toString();
+        String preparationInstructions = editTextPreparationInstructions.getText().toString();
+
+        if (title.trim().isEmpty() || ingredients.trim().isEmpty() || preparationInstructions.trim().isEmpty()) {
+            Toast.makeText(this, getString(R.string.emptyFieldsWarning), Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Intent data = new Intent();
+        data.putExtra(EXTRA_TITLE, title);
+        data.putExtra(EXTRA_PORTIONS_NUM, portionNumber);
+        data.putExtra(EXTRA_INGREDIENTS, ingredients);
+        data.putExtra(EXTRA_PREP_INSTRUCTIONS, preparationInstructions);
+
+        setResult(RESULT_OK, data);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_recipe_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_recipe:
+                saveRecipe();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
--- app/src/main/java/com/example/zeszytmojejmamy/MainActivity.java
+++ app/src/main/java/com/example/zeszytmojejmamy/MainActivity.java
@@ -1,23 +1,38 @@
 package com.example.zeszytmojejmamy;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private RecipeViewModel recipeViewModel;
+    public static final int ADD_RECIPE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton buttonAddRecipe = findViewById(R.id.button_add_recipe);
+        buttonAddRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddRecipeActivity.class);
+                startActivityForResult(intent, ADD_RECIPE_REQUEST);
+            }
+        });
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -35,5 +50,25 @@ public class MainActivity extends AppCompatActivity {
                 //Toast.makeText(MainActivity.this,"onChanged", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ADD_RECIPE_REQUEST && resultCode == RESULT_OK) {
+            String title = data.getStringExtra(AddRecipeActivity.EXTRA_TITLE);
+            String portions = data.getStringExtra(AddRecipeActivity.EXTRA_PORTIONS_NUM);
+            //data.getIntExtra(EXTRA, default num)
+            String ingredients = data.getStringExtra(AddRecipeActivity.EXTRA_INGREDIENTS);
+            String preparationInstructions = data.getStringExtra(AddRecipeActivity.EXTRA_PREP_INSTRUCTIONS);
+
+            Recipe recipe = new Recipe(title, null, ingredients, portions, preparationInstructions, null, null);
+            recipeViewModel.insert(recipe);
+
+            Toast.makeText(this, getString(R.string.recipeSaved), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, getString(R.string.recipeNotSaved), Toast.LENGTH_SHORT).show();
+        }
     }
 }
